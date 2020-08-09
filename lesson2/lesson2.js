9bacc0c46684621de7ef77419e1043a817ef3704
Katerina Frishman <ekaterinaorlova7@gmail.com>
2020-08-09 21:44:57 +0300

    Use array

--- lesson2/lesson2.js
+++ lesson2/lesson2.js
@@ -1,4 +1,5 @@
-let a=Math.max(5,7,10);
-let b=Math.min(5,7,10);
-alert("Maximum number: "+a);
-alert("Minimum number: "+b);
+const numbers = [5,10,7];
+const a = Math.max(...numbers);
+const b = Math.min(...numbers);
+alert("Maximum number: " + a);
+alert("Minimum number: " + b);
